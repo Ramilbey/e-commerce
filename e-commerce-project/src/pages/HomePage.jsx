a3066ccab7d629082c56d400bfdad445b13f4e28
Ramilbey <ramilbey505711503@gmail.com>
2025-05-04 23:17:26 +0800

    solve bug by moving header css to components

--- e-commerce-project/src/pages/HomePage.jsx
+++ e-commerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { products } from "../../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            console.log(response)
+            
+        })
+
+
   return (
     <>
       <title>Ecommerce Project</title>
