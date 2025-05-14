daffc96d9835f22d94e2f8df65a4942f876cabd1
Ramilbey <ramilbey505711503@gmail.com>
2025-05-14 23:55:25 +0800

    remove default dayjs

--- e-commerce-project/src/pages/checkout/CheckoutPage.jsx
+++ e-commerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import dayjs from "dayjs";
 import { formatMoney } from "../../utilis/money";
 import { useEffect, useState } from "react";
 import { OrderSummary } from "./OrderSummary";
