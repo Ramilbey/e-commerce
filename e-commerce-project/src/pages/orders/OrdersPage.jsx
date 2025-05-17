fbc6aaa9c628e2c964bcfdc40b8dc143e6c3833d
Ramilbey <ramilbey505711503@gmail.com>
2025-05-17 19:19:17 +0800

    Refactor: replaced .then() with async/await in useEffect to fetch cart items

--- e-commerce-project/src/pages/orders/OrdersPage.jsx
+++ e-commerce-project/src/pages/orders/OrdersPage.jsx
@@ -9,9 +9,10 @@ import "./OrdersPage.css";
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
   useEffect(() => {
-    axios.get("/api/orders?expand=products").then((response) => {
-      setOrders(response.data);
-    });
+    const fetchOrdersData = async () => {
+      const response = axios.get("/api/orders?expand=products")
+        setOrders(response.data); 
+    }
   }, []);
   return (
     <>
