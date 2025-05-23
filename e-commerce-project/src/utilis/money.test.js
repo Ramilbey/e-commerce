fbaa9fba56dde58311fe165e14bd90c6954ec267
Ramilbey <ramilbey505711503@gmail.com>
2025-05-23 19:28:29 +0800

    add mutliple checks

--- e-commerce-project/src/utilis/money.test.js
+++ e-commerce-project/src/utilis/money.test.js
@@ -7,6 +7,7 @@ it("formats 1999 cents as $19.99", () => {
 });
 
 it("display 2 decimals", () => {
-  //create our second test  
+  //create our second test
   expect(formatMoney(1090)).toBe("$10.90");
+  expect(formatMoney(100)).toBe("$1.00"); // mutliple checks
 });
