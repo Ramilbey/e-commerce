48c5bfded6367d5e8691f72bac645b2938a31b70
Ramilbey <ramilbey505711503@gmail.com>
2025-05-23 19:30:54 +0800

    /create a test suite for clean code

--- e-commerce-project/src/utilis/money.test.js
+++ e-commerce-project/src/utilis/money.test.js
@@ -1,13 +1,16 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { formatMoney } from "./money";
 
-it("formats 1999 cents as $19.99", () => {
-  //create our first test
-  expect(formatMoney(1999)).toBe("$19.99");
-});
+//create a test suite for clean code
+describe("formatMoney", () => {
+  it("formats 1999 cents as $19.99", () => {
+    //create our first test
+    expect(formatMoney(1999)).toBe("$19.99");
+  });
 
-it("display 2 decimals", () => {
-  //create our second test
-  expect(formatMoney(1090)).toBe("$10.90");
-  expect(formatMoney(100)).toBe("$1.00"); // mutliple checks
+  it("display 2 decimals", () => {
+    //create our second test
+    expect(formatMoney(1090)).toBe("$10.90");
+    expect(formatMoney(100)).toBe("$1.00"); // mutliple checks
+  });
 });
