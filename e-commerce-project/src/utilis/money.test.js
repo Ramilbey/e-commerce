51ed73a3a0d7e45bd59cc856e0aba16058fc5bc7
Ramilbey <ramilbey505711503@gmail.com>
2025-05-23 19:20:23 +0800

    test format money

--- e-commerce-project/src/utilis/money.test.js
+++ e-commerce-project/src/utilis/money.test.js
@@ -1,2 +1,6 @@
-import it from 'vitest';
+import {it, expect } from 'vitest';
 import { formatMoney } from './money';
+
+it('formats 1999 cents as $19.99', ()=> {
+    expect(formatMoney(1999)).toBe('$19.99')
+})
