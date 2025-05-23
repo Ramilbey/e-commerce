82289838cfefa906f9f035e56677f1e3ad2d78e6
Ramilbey <ramilbey505711503@gmail.com>
2025-05-23 19:26:23 +0800

    create second test for format money

--- e-commerce-project/src/utilis/money.test.js
+++ e-commerce-project/src/utilis/money.test.js
@@ -1,6 +1,12 @@
-import {it, expect } from 'vitest';
-import { formatMoney } from './money';
+import { it, expect } from "vitest";
+import { formatMoney } from "./money";
 
-it('formats 1999 cents as $19.99', ()=> {
-    expect(formatMoney(1999)).toBe('$19.99')
-})
+it("formats 1999 cents as $19.99", () => {
+  //create our first test
+  expect(formatMoney(1999)).toBe("$19.99");
+});
+
+it("display 2 decimals", () => {
+  //create our second test  
+  expect(formatMoney(1090)).toBe("$10.90");
+});
